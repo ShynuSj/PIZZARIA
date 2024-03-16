bbfc1f45039177d0b04fd7150c17255273b64e36
shynu S.J <shynujose11@gmail.com>
2024-03-16 13:39:31 +0400

    data fetching centralised with routing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,5 +19,6 @@ module.exports = {
     "no-unused-vars": "off",
     "react/no-unescaped-entities": "off",
     "react-refresh/only-export-components": "off",
+    "react/prop-types": "off",
   },
 };
